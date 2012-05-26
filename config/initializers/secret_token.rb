4614edb742d5c2e85c7fed4a40bd7e0396029a75
Mingding <mingding@playmoolah.com>
2012-05-26 13:26:59 +0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SnapNSave::Application.config.secret_token = '92f0e9644899b139ffdfc6c86752856a07dca2300017b3632022ab2ad7615805f0ec5e10c4ff973cf70511b3495ce6a7f215d9c18a9edd16241847ab42bcd4a5'
