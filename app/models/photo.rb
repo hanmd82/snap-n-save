3a8db086b3b00d8f89f7d66e6c044a88ba9ed817
Mingding <mingding@playmoolah.com>
2012-06-02 00:24:43 +0800

    Added lat/lng filtering to nearby locations

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,6 +1,15 @@
 class Photo < ActiveRecord::Base
+
+  COORDINATE_DELTA = 0.05
+
   has_attached_file :image,
                     :styles => { :thumbnail => "100x100#" },
                     :storage => :s3,
                     :s3_credentials => S3_CREDENTIALS
+
+  scope :nearby, lambda { |lat, lng|
+    where("lat BETWEEN ? AND ?", lat - COORDINATE_DELTA, lat + COORDINATE_DELTA).
+    where("lng BETWEEN ? AND ?", lng - COORDINATE_DELTA, lng + COORDINATE_DELTA).
+    limit(64)
+  }
 end
