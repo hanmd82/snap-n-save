a2cb74d187f1828903b03cb19aee76ad9c4d072b
Mingding <mingding@playmoolah.com>
2012-06-10 22:14:23 +0800

    Turned off nearby location filtering and allowed POST API call to bypass CSRF authenticity check

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,14 +1,18 @@
 class PhotosController < ApplicationController
+  skip_before_filter :verify_authenticity_token, :only => :create
+
   respond_to :json
 
   def index
-    lat, lng = params[:lat], params[:lng]
-    if lat and lng
-      @photos = Photo.nearby(lat.to_f, lng.to_f)
-      respond_with({:photos => @photos})
-    else
-      respond_with({:message => "Invalid or missing lat/lng parameters"}, :status => 406)
-    end
+    # lat, lng = params[:lat], params[:lng]
+    # if lat and lng
+    #   @photos = Photo.nearby(lat.to_f, lng.to_f)
+    #   respond_with({:photos => @photos})
+    # else
+    #   respond_with({:message => "Invalid or missing lat/lng parameters"}, :status => 406)
+    # end
+    @photos = Photo.limit(64)
+    respond_with({:photos => @photos})
   end
 
   def show
